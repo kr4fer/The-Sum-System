b48cf1e815f4b2efc3a97525661a2ffcbb788755
Sunty Eljohn <163456176+kr4fer@users.noreply.github.com>
2025-05-26 18:38:20 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout<<"         Enter value 1: ";
+    cin>>num1;
+    cout<<"         Enter Value 2: ";
+    cin>>num2;
+
+    sum = num1 + num2;
+
+    cout<<endl<<endl<<endl;
+
+    cout<<"         The total sum is: "<<sum;
+
+    return 0;
+}
